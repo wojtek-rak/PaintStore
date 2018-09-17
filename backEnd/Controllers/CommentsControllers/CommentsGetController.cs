6725510c8392af921882863c262a8b27a6b2f666
Wolix <Rak.wojtek@wp.pl>
2018-09-17 22:42:02 +0200

    Revert "Revert "Comments better response""
    This reverts commit 6fbf36ce6a9c7d585b7a6b0ee8bfe40b59c2b0a2.

--- backEnd/Controllers/CommentsControllers/CommentsGetController.cs
+++ backEnd/Controllers/CommentsControllers/CommentsGetController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backEnd.Models;
+using backEnd.Models.ResultsModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,12 +24,18 @@ namespace backEnd.Controllers
         }
 
         [HttpPost]
-        public IEnumerable<PostComments> GetComments([FromBody] Posts post)//([FromBody] string imgLink)
+        public IEnumerable<PostCommentsResult> GetComments([FromBody] Posts post)//([FromBody] string imgLink)
         {
+            List<PostCommentsResult> commentsResult = new List<PostCommentsResult>();
             using (var db = paintStoreContext)
             {
                 var comments = db.PostComments.Where(b => b.PostId == post.Id).OrderByDescending(x => x.LikeCount);
-                return comments.ToList();
+                foreach (var comment in comments)
+                {
+                    var userAvatarImgLink = db.Users.First(x => x.Id == comment.UserId).AvatarImgLink;
+                    commentsResult.Add(new PostCommentsResult(comment){UserAvatarImgLink = userAvatarImgLink});
+                }
+                return commentsResult;
             }
 
         }

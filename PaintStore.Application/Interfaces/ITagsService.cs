fa7b0f5b08daaec2a94d1b41ebc1be26be1e18df
Wojciech Rak <rak.wojtek@wp.pl>
2019-03-11 23:27:08 +0100

    Repair tags

--- PaintStore.Application/Interfaces/ITagsService.cs
+++ PaintStore.Application/Interfaces/ITagsService.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using PaintStore.Domain.Entities;
+using PaintStore.Persistence;
 
 namespace PaintStore.Application.Interfaces
 {
     public interface ITagsService
     {
         List<Tags> GetTags(int postId);
-        Tags GetOrAddTag(string tagName);
+        Tags GetOrAddTag(string tagName, PaintStoreContext db);
         int AddPostTags(List<string> tagsList, int postId);
 
     }
